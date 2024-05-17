b2fa28f5c4bdc7bf46a9bab0834a2bec6e47188d
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-17 13:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+function solve(num,mapp) {
+	if(num==1 || num==2) return num-1;
+	
+	if(mapp.has(num)) return mapp.get(num);
+	
+	const ans= solve(num-1,mapp)+solve(num-2,mapp);
+	mapp.set(num,ans);
+	
+	return ans;
+}
+
 function fibonacci(num) {
 // your code here
-	if(num==1 || num==2) return num-1;
-
-	return fibonacci(num-1)+fibonacci(num-2);
-	
+	const mapp=new Map();
+	return solve(num,mapp);
 }
 
 module.exports = fibonacci;
